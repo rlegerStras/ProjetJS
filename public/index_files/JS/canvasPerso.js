8fe32e82c4c0dba4faf2edd472ddc83243b319c2
Rominho15 <roromw8@gmail.com>
2015-10-14 15:04:08 +0200

    - Restructuration dossiers et fichiers - Création de la musique (fichiers dans le dossier public/index_files/Music/*.mp3), non visile (.gitignore) - Nouvelle gestion des couleurs (aléatoires) - Déplacement des vies dans le coté droit de la page web

--- public/index_files/JS/canvasPerso.js
+++ public/index_files/JS/canvasPerso.js
@@ -11,7 +11,9 @@ var ws,
     yInit = 0,
     tailleCercle = 10,
     nbDisqueIni = 8,
-    snakes = [];
+    snakes = [],
+    couleurs = ["#F0F8FF", "#FAEBD7", "#00FFFF", "#7FFFD4", "#F0FFFF", "#F5F5DC", "#FFE4C4", "#000000", "#FFEBCD", "#0000FF", "#8A2BE2", "#A52A2A", "#DEB887", "#5F9EA0", "#7FFF00", "#D2691E", "#FF7F50", "#6495ED", "#FFF8DC", "#DC143C", "#00FFFF", "#00008B", "#008B8B", "#B8860B", "#A9A9A9", "#006400", "#BDB76B", "#8B008B", "#556B2F", "#FF8C00", "#9932CC", "#8B0000", "#E9967A", "#8FBC8F", "#483D8B", "#2F4F4F", "#00CED1", "#9400D3", "#FF1493", "#00BFFF", "#696969", "#1E90FF", "#B22222", "#FFFAF0", "#228B22", "#FF00FF", "#DCDCDC", "#F8F8FF", "#FFD700", "#DAA520", "#808080", "#008000", "#ADFF2F", "#F0FFF0", "#FF69B4", "#CD5C5C", "#4B0082", "#FFFFF0", "#F0E68C", "#E6E6FA", "#FFF0F5", "#7CFC00", "#FFFACD", "#ADD8E6", "#F08080", "#E0FFFF", "#FAFAD2", "#90EE90", "#D3D3D3", "#FFB6C1", "#FFA07A", "#20B2AA", "#87CEFA", "#778899", "#B0C4DE", "#FFFFE0", "#00FF00", "#32CD32", "#FAF0E6", "#FF00FF", "#800000", "#66CDAA", "#0000CD", "#BA55D3", "#9370DB", "#3CB371", "#7B68EE", "#00FA9A", "#48D1CC", "#C71585", "#191970", "#F5FFFA", "#FFE4E1", "#FFE4B5", "#FFDEAD", "#000080", "#FDF5E6", "#808000", "#6B8E23", "#FFA500", "#FF4500", "#DA70D6", "#EEE8AA", "#98FB98", "#AFEEEE", "#DB7093", "#FFEFD5", "#FFDAB9", "#CD853F", "#FFC0CB", "#DDA0DD", "#B0E0E6", "#800080", "#FF0000", "#BC8F8F", "#4169E1", "#8B4513", "#FA8072", "#F4A460", "#2E8B57", "#FFF5EE", "#A0522D", "#C0C0C0", "#87CEEB", "#6A5ACD", "#708090", "#FFFAFA", "#00FF7F", "#4682B4", "#D2B48C", "#008080", "#D8BFD8", "#FF6347", "#40E0D0", "#EE82EE", "#F5DEB3", "#FFFFFF", "#F5F5F5", "#FFFF00", "9ACD32"],
+    indCouleur = Math.floor(Math.random() * (couleurs.length + 1));
 
 ws = new WebSocket('wss://localhost:3000');
 
@@ -23,24 +25,26 @@ function createCircle(nombre, disques) {
     
     yInit = Math.random() * (maxY + 1);
     
-    console.log(snakes.length);
     // Tête
     if (nombre === 0) {
         xVal = xInit;
         yVal = yInit;
-        colorVal = 'green';
+        colorVal = "black";
     } else {
         // Corps
         xVal = disques[nombre - 1].x - tailleCercle;
         yVal = disques[nombre - 1].y;
-        colorVal = 'blue';
+        colorVal = couleurs[indCouleur];
     }
+    
+    
     // Sous format x,y,color
     disque = {
         x : xVal,
         y : yVal,
         color : colorVal
     };
+    console.log(indCouleur);
     return disque;
 }
 
@@ -79,8 +83,10 @@ function createSnakeServer() {
         histo : histo,
         directionX : directionX,
         directionY : directionY,
-        vies : 1
+        vies : 5
     };
+    
+    indCouleur = Math.random() * (couleurs.length + 1);
     
     return snake;
 }
@@ -100,6 +106,7 @@ function createSnakePaper(newSnake) {
             fillColor: newSnake.disques[i].color,
             strokeColor: 'black'
         });
+        console.log(newSnake.disques[i].color);
         disques.push(disque);
     }
     // Ajout en variable globale (dans snakes)
@@ -116,7 +123,7 @@ function update(UpSnakes) {
     var is,
         i,
         p = document.getElementsByTagName("p")[0],
-        str = "Scores : <br/>";
+        str = "Scores<br/>";
     
     // Mise à jour des nouvelles coordonnées par le serveur
     for (is = 0; is < UpSnakes.length; is = is + 1) {
@@ -125,7 +132,7 @@ function update(UpSnakes) {
                 snakes[is].disques[i].position.x = UpSnakes[is].disques[i].x;
                 snakes[is].disques[i].position.y = UpSnakes[is].disques[i].y;
             }
-            str = str + "Utilisateur " + is + " : " + UpSnakes[is].vies + " vies<br/>";
+            str = str + "Player " + is + " : " + UpSnakes[is].vies + "<br/>";
         }
     }
     p.innerHTML = str;
@@ -198,7 +205,6 @@ function onMouseDown(event) {
     ws.send('clic' + Jpoint);
 }
 
-/*//Music
+//Music
 var player = document.querySelector('#audioPlayer');
 player.play();
-*/
